6afa643b76f3b1618b5373830795e268603b773b
dannidanna <dannidanna01@gmail.com>
2015-09-15 14:07:37 -0400

    Interfaz GR

--- GeneradorAutomatas/src/Interface/GREditor.java
+++ GeneradorAutomatas/src/Interface/GREditor.java
@@ -28,21 +28,248 @@ public class GREditor extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        buttonAEFD = new javax.swing.JButton();
+        buttonAEFND = new javax.swing.JButton();
+        buttonER = new javax.swing.JButton();
+        btnGrabar = new javax.swing.JButton();
+        btnRecuperar = new javax.swing.JButton();
+        btnVerificar = new javax.swing.JButton();
+        txtTerminal = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        txtCadena1 = new javax.swing.JTextField();
+        btnAniadir = new javax.swing.JButton();
+        bttNuevo = new javax.swing.JButton();
+        btnTerminal = new javax.swing.JButton();
+        btnNoTerminales = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        txtMain = new javax.swing.JTextPane();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        buttonAEFD.setText("AEFD");
+        buttonAEFD.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonAEFDActionPerformed(evt);
+            }
+        });
+
+        buttonAEFND.setText("AEFND");
+        buttonAEFND.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonAEFNDActionPerformed(evt);
+            }
+        });
+
+        buttonER.setText("AEFND");
+        buttonER.setActionCommand("ER");
+        buttonER.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonERActionPerformed(evt);
+            }
+        });
+
+        btnGrabar.setText("GRABAR");
+        btnGrabar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnGrabarActionPerformed(evt);
+            }
+        });
+
+        btnRecuperar.setText("RECUPERAR");
+        btnRecuperar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRecuperarActionPerformed(evt);
+            }
+        });
+
+        btnVerificar.setText("VERIFICAR");
+        btnVerificar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnVerificarActionPerformed(evt);
+            }
+        });
+
+        txtTerminal.setText("T");
+        txtTerminal.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtTerminalActionPerformed(evt);
+            }
+        });
+
+        jLabel3.setText("--->");
+
+        txtCadena1.setText("A b c d");
+
+        btnAniadir.setText("+");
+        btnAniadir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAniadirActionPerformed(evt);
+            }
+        });
+
+        bttNuevo.setText("Nuevo");
+        bttNuevo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bttNuevoActionPerformed(evt);
+            }
+        });
+
+        btnTerminal.setText("TERMINAL");
+        btnTerminal.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnTerminalActionPerformed(evt);
+            }
+        });
+
+        btnNoTerminales.setText("NO TERMINAL");
+        btnNoTerminales.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnNoTerminalesActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("No terminal");
+
+        jLabel2.setText("cadena (utilizar espacio)");
+
+        jScrollPane1.setViewportView(txtMain);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(24, 24, 24)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(txtTerminal)
+                                    .addComponent(buttonAEFD, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(buttonAEFND)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                        .addComponent(buttonER))
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addGap(22, 22, 22)
+                                        .addComponent(jLabel3)
+                                        .addGap(18, 18, 18)
+                                        .addComponent(txtCadena1, javax.swing.GroupLayout.PREFERRED_SIZE, 114, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addGap(60, 60, 60)
+                                        .addComponent(jLabel2))))
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(33, 33, 33)
+                        .addComponent(btnGrabar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnRecuperar)
+                        .addGap(25, 25, 25)))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnVerificar)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(13, 13, 13)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(btnTerminal, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(btnAniadir)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(bttNuevo))
+                            .addComponent(btnNoTerminales, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(buttonAEFD)
+                    .addComponent(buttonAEFND)
+                    .addComponent(buttonER))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel2))
+                        .addGap(1, 1, 1)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(txtTerminal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel3)
+                            .addComponent(txtCadena1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(btnAniadir)
+                            .addComponent(bttNuevo))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 125, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(btnGrabar)
+                            .addComponent(btnRecuperar)
+                            .addComponent(btnVerificar))
+                        .addGap(23, 23, 23))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(83, 83, 83)
+                        .addComponent(btnTerminal)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnNoTerminales)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void buttonAEFNDActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonAEFNDActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_buttonAEFNDActionPerformed
+
+    private void buttonERActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonERActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_buttonERActionPerformed
+
+    private void btnGrabarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGrabarActionPerformed
+       
+
+    }//GEN-LAST:event_btnGrabarActionPerformed
+
+    private void btnRecuperarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRecuperarActionPerformed
+        
+    }//GEN-LAST:event_btnRecuperarActionPerformed
+
+    private void btnVerificarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVerificarActionPerformed
+        // TODO add your handling code here:
+        //comprobar sintaxis
+
+    }//GEN-LAST:event_btnVerificarActionPerformed
+
+    private void txtTerminalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtTerminalActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtTerminalActionPerformed
+
+    private void btnAniadirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAniadirActionPerformed
+
+    }//GEN-LAST:event_btnAniadirActionPerformed
+
+    private void bttNuevoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bttNuevoActionPerformed
+        // Borra todos los datos actuales
+    }//GEN-LAST:event_bttNuevoActionPerformed
+
+    private void btnTerminalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTerminalActionPerformed
+        // 
+    }//GEN-LAST:event_btnTerminalActionPerformed
+
+    private void btnNoTerminalesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNoTerminalesActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnNoTerminalesActionPerformed
+
+    private void buttonAEFDActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonAEFDActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_buttonAEFDActionPerformed
 
     /**
      * @param args the command line arguments
@@ -80,5 +307,22 @@ public class GREditor extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAniadir;
+    private javax.swing.JButton btnGrabar;
+    private javax.swing.JButton btnNoTerminales;
+    private javax.swing.JButton btnRecuperar;
+    private javax.swing.JButton btnTerminal;
+    private javax.swing.JButton btnVerificar;
+    private javax.swing.JButton bttNuevo;
+    private javax.swing.JButton buttonAEFD;
+    private javax.swing.JButton buttonAEFND;
+    private javax.swing.JButton buttonER;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField txtCadena1;
+    private javax.swing.JTextPane txtMain;
+    private javax.swing.JTextField txtTerminal;
     // End of variables declaration//GEN-END:variables
 }
